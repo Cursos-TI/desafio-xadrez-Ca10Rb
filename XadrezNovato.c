4ade2d454109b11ba67599263ee812d3d69acba4
Ca10Rb <caioroberto0459@gmail.com>
2025-05-22 11:59:06 +0000

    Nível aventureiro

--- XadrezNovato.c
+++ XadrezNovato.c
@@ -5,27 +5,49 @@ int main(){
     int rainha = 1;
     int bispo = 1;
     int torre = 1;
+    int cavalo = 1;
     
 // Movimento da torre
     while (torre <= 5)
     {
-        printf("Uma casa para Direita\n");
+        printf("Direita\n");
         torre++;
     }
 
 // Movimento do bispo
     do
     {
-        printf("Uma casa para Cima e uma casa para Direita\n");
+        printf("Cima e Direita\n");
         bispo++;
     } while (bispo <= 5);
 
 // Movimento da rainha
     for (rainha = 1; rainha <= 8; rainha++)
     {
-        printf("Uma casa para esquerda\n");
+        printf("Esquerda\n");
     }
     
+// Movimento do cavalo
+   int passobaixo = 2;
+   int passoesquerda = 1;
+   int passoatual = 0;
+
+   while (cavalo == 1)
+   {
     
+      for (int i = 0; i < passobaixo; i++)
+    {
+        printf("Baixo\n");
+    }
+     
+      while (passoatual < passoesquerda)
+      {
+        printf("Esquerda\n");
+        passoatual++;
+      }
+    cavalo++; // Evitar loop infinito
+   }
+   
 return 0;
 }
+
